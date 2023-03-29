235f1c1c76ccb5aa37b52483cdb600770ef8d2b2
Christoph Fink <christoph.fink@gmail.com>
2023-03-29 21:08:29 +0300

    first commit

---
+++ src/assets/js/christophfink.com.js
@@ -0,0 +1,14 @@
+/* globals */
+/* eslint-disable object-shorthand, object-curly-newline, no-multi-assign, consistent-this, radix, no-global-assign, no-native-reassign */
+/* eslint no-console: "warn" */
+/* eslint max-depth: ["warn", 6] */
+/* eslint no-unused-vars: ["error", { "vars": "local" } ] */
+
+(function () {
+    "use strict"
+
+    document.addEventListener("DOMContentLoaded", function () {
+
+    })
+
+}())
---
+++ upload.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set +m -euo pipefail
+IFS=$'\n\t '
+
+declare SRC="$(dirname $0)/src/"
+declare BUILD="$(dirname $0)/build/"
+declare REMOTE="christoph@[fdcf::1]:/var/www/dev.christophfink.com/htdocs-secure/"
+
+# 1. sync to build dir and generate css from sass
+rsync \
+    --archive \
+    --info=progress2 \
+    --delete \
+    --copy-unsafe-links \
+    --exclude="assets/sass/" \
+    "${SRC}" \
+    "${BUILD}"
+
+mkdir -p "${BUILD}/assets/css/"
+
+sassc \
+    --sass \
+    --style compressed \
+    "${SRC}/assets/sass/christophfink.com.sass" \
+    "${BUILD}/assets/css/christophfink.com.css"
+
+# 2. sync to server
+rsync \
+    --archive \
+    --info=progress2 \
+    --exclude="urbanage-ttm-ui" \
+    --delete \
+    "${BUILD}" \
+    "${REMOTE}"
